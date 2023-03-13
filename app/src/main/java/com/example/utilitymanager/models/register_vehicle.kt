47914200baeab7608f5f32c66702f005f43c1829
IT21006784 <it21006784@my.sliit.lk>
2023-03-14 02:03:11 +0530

    Fuel2

--- app/src/main/java/com/example/utilitymanager/models/register_vehicle.kt
+++ app/src/main/java/com/example/utilitymanager/models/register_vehicle.kt
@@ -14,10 +14,7 @@ class register_vehicle : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(com.example.utilitymanager.R.layout.activity_register_vehicle)
 
-        val dropdown = findViewById<Spinner>(R.id.list)
-        val items = arrayOf("Toyota", "Suzuki", "Nissan", "Hyundai", "BMW", "Audi", "Honda", "other")
-        val adapter = ArrayAdapter(this, R.layout.simple_spinner_dropdown_item, items)
-        dropdown.adapter = adapter
-
     }
+
+
 }
