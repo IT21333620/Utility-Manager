2641799820f7383a4b3eeb38939f3a88cbf43016
IT21006784 <it21006784@my.sliit.lk>
2023-03-14 20:53:03 +0530

    fuel3

---
+++ app/src/main/java/com/example/utilitymanager/models/weeklyfuel_usage.kt
@@ -0,0 +1,37 @@
+package com.example.utilitymanager.models
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.example.utilitymanager.R
+
+class weeklyfuel_usage : AppCompatActivity() {
+    private val users = arrayOf("User", "Company", "Advertiser")
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weeklyfuel_usage)
+
+        val spinner2 = findViewById<Spinner>(R.id.spinner2)
+        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, users)
+        spinner2.adapter = arrayAdapter
+        spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                //Toast.makeText(applicationContext, "selected player is: " + users[position], Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+}}
