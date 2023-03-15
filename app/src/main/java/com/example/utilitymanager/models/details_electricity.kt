727cc151281dbb398a47f4732a6ba6e078c07363
IT21333620 <it21333620@my.sliit.lk>
2023-03-16 00:49:05 +0530

    Init Commit

---
+++ app/src/main/java/com/example/utilitymanager/models/details_electricity.kt
@@ -0,0 +1,12 @@
+package com.example.utilitymanager.models
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.utilitymanager.R
+
+class details_electricity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_electricity)
+    }
+}
