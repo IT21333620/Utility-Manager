8e60c4d542f51b8bb3c67739e38e7db69a0b7241
IT21037092 <it21037092@my.sliit.lk>
2023-03-17 15:11:31 +0530

    init commit

---
+++ app/src/main/java/com/example/utilitymanager/models/Add_gas_details.kt
@@ -0,0 +1,12 @@
+package com.example.utilitymanager.models
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.utilitymanager.R
+
+class Add_gas_details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_gas_details)
+    }
+}
---
+++ app/src/main/java/com/example/utilitymanager/models/Add_today_screentime.kt
@@ -0,0 +1,12 @@
+package com.example.utilitymanager.models
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.utilitymanager.R
+
+class Add_today_screentime : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_today_screentime)
+    }
+}
