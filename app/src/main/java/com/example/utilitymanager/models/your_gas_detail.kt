7b1bc6dd5a4aeb6ae92efe18384ff3758d7b3014
IT21037092 <it21037092@my.sliit.lk>
2023-03-19 20:05:04 +0530

    init commit

---
+++ app/src/main/java/com/example/utilitymanager/models/your_gas_detail.kt
@@ -0,0 +1,12 @@
+package com.example.utilitymanager.models
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.utilitymanager.R
+
+class your_gas_detail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_your_gas_detail)
+    }
+}
