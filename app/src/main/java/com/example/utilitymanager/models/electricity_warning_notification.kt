cd583bbb4c9046e1b212ac2b554eff7055e7e6ff
IT21333620 <it21333620@my.sliit.lk>
2023-03-19 18:58:36 +0530

    Init Commit dimen

---
+++ app/src/main/java/com/example/utilitymanager/models/electricity_warning_notification.kt
@@ -0,0 +1,11 @@
+package com.example.utilitymanager.models
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class electricity_warning_notification : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_electricity_warning_notification)
+    }
+}
