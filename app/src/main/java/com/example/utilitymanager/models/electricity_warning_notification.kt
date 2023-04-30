0d621823a48cff4015c9acf724f2549805e9df2f
IT21333620 <it21333620@my.sliit.lk>
2023-04-30 11:00:47 +0530

    fix errors

--- app/src/main/java/com/example/utilitymanager/models/electricity_warning_notification.kt
+++ app/src/main/java/com/example/utilitymanager/models/electricity_warning_notification.kt
@@ -2,6 +2,7 @@ package com.example.utilitymanager.models
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.utilitymanager.R
 
 class electricity_warning_notification : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
