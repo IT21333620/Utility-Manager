39e107e15997437fd518c763dc14faa77259893e
IT21333620 <it21333620@my.sliit.lk>
2023-05-01 12:28:42 +0530

    fireBase Auth

--- app/src/main/java/com/example/utilitymanager/models/register.kt
+++ app/src/main/java/com/example/utilitymanager/models/register.kt
@@ -2,11 +2,23 @@ package com.example.utilitymanager.models
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.utilitymanager.R
+import com.google.android.material.textfield.TextInputEditText
 
 class register : AppCompatActivity() {
+
+    private lateinit var enterEmail: TextInputEditText
+    private lateinit var enterPassword: TextInputEditText
+    private lateinit var reEnterPassword: TextInputEditText
+    private lateinit var register: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        enterEmail = findViewById<TextInputEditText>(R.id.loginPassword)
+
+
+
     }
 }
